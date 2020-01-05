f1350ff24751ff82d464023aac5debec24bc347a
Yohann Jolain <yohann.51300@hotmail.fr>
2020-01-05 18:46:47 +0100

    Added all single variable graph

--- CategoricalAnalyze.R
+++ CategoricalAnalyze.R
@@ -22,31 +22,245 @@ require(ggplot2)
 require(lubridate)
 require(scales)
 require(usmap)
+require(grid)
 
 rm(list = ls())
 
 shootings <- read_csv(file = "./US_Shootings_Cleaned.csv")
+
+##################
+## Gender study ##
+##################
+
 shootings.Gender <- shootings %>% 
   group_by(Gender) %>% 
   count() %>% 
   ungroup() %>% 
-  mutate(Shootings=`n`/sum(`n`)) %>% 
+  mutate(ShootingsPct=`n`/sum(`n`)) %>% 
+  mutate(Shootings=`n`) %>% 
   arrange(desc(Gender))
-shootings.Gender$Label <- scales::percent(shootings.Gender$Shootings)
+shootings.Gender$Label <- scales::percent(shootings.Gender$ShootingsPct)
+
+ggplot(shootings.Gender, aes(x="", y=ShootingsPct, fill=Gender))+
+  geom_bar(width = 1, stat = "identity") +
+  coord_polar("y", start=0) +
+  theme(axis.text.x=element_blank()) +
+  geom_text(aes(x=1, y = cumsum(ShootingsPct) - ShootingsPct/3, label=Label)) +
+  ggtitle("US Shootings Frequencies By Gender")
+
+#####################
+## Ethnicity study ##
+#####################
 
 shootings.Race <- shootings %>% 
   group_by(Race) %>% 
   count() %>% 
   ungroup() %>% 
-  mutate(Shootings=`n`/sum(`n`)) %>% 
+  mutate(ShootingsPct=`n`/sum(`n`)) %>% 
+  mutate(Shootings=`n`) %>% 
   arrange(desc(Race))
-shootings.Race$Label <- scales::percent(shootings.Race$Shootings)
+shootings.Race$Label <- scales::percent(shootings.Race$ShootingsPct)
+
+ggplot(shootings.Race, 
+       aes(x = reorder(Race, -ShootingsPct),
+           y = ShootingsPct)) + 
+  geom_bar(stat = "identity", 
+           fill = "lightblue", 
+           color = "black") +
+  geom_text(aes(label = Label), 
+            hjust = -0.15) +
+  scale_y_continuous(labels = percent) +
+  labs(x = "Race", 
+       y = "Percent", 
+       title  = "US Shootings Frequencies By Ethnicity") +
+  coord_flip()
+
+#########################
+## Incident area study ##
+#########################
+
+shootings.IncidentArea <- shootings %>% 
+  group_by(Incident.Area) %>% 
+  count() %>% 
+  ungroup() %>% 
+  mutate(ShootingsPct=`n`/sum(`n`)) %>% 
+  mutate(Shootings=`n`) %>% 
+  arrange(desc(Incident.Area))
+shootings.IncidentArea$Label <- scales::percent(shootings.IncidentArea$ShootingsPct)
+
+ggplot(shootings.IncidentArea, 
+       aes(x = reorder(Incident.Area, -ShootingsPct),
+           y = ShootingsPct)) + 
+  geom_bar(stat = "identity", 
+           fill = "lightblue", 
+           color = "black") +
+  geom_text(aes(label = Label), 
+            hjust = -0.15) +
+  scale_y_continuous(labels = percent) +
+  labs(x = "Area of incident", 
+       y = "Percent", 
+       title  = "US Shootings Frequencies By Area of incident") +
+  coord_flip()
+
+
+######################
+## Open Close study ##
+######################
+
+shootings.OpenClose <- shootings %>% 
+  group_by(Open.Close.Location) %>% 
+  count() %>% 
+  ungroup() %>% 
+  mutate(ShootingsPct=`n`/sum(`n`)) %>% 
+  mutate(Shootings=`n`) %>% 
+  arrange(desc(Open.Close.Location))
+shootings.OpenClose$Label <- scales::percent(shootings.OpenClose$ShootingsPct)
+
+ggplot(shootings.OpenClose, aes(x="", y=ShootingsPct, fill=Open.Close.Location))+
+  geom_bar(width = 1, stat = "identity") +
+  coord_polar("y", start=0) +
+  theme(axis.text.x=element_blank()) +
+  geom_text(aes(x=1, y = cumsum(ShootingsPct) - ShootingsPct/3, label=Label)) +
+  ggtitle("US Shootings Frequencies By Accessibility")
+
+##################
+## Target study ##
+##################
+
+shootings.Target <- shootings %>% 
+  group_by(Target) %>% 
+  count() %>% 
+  ungroup() %>% 
+  mutate(ShootingsPct=`n`/sum(`n`)) %>% 
+  mutate(Shootings=`n`) %>% 
+  arrange(desc(Target))
+shootings.Target$Label <- scales::percent(shootings.Target$ShootingsPct)
+
+ggplot(shootings.Target, 
+       aes(x = reorder(Target, -ShootingsPct),
+           y = ShootingsPct)) + 
+  geom_bar(stat = "identity", 
+           fill = "lightblue", 
+           color = "black") +
+  geom_text(aes(label = Label), 
+            hjust = -0.15) +
+  scale_y_continuous(labels = percent) +
+  labs(x = "Type of target", 
+       y = "Percent", 
+       title  = "US Shootings Frequencies By Target") +
+  coord_flip()
+
+#################
+## Cause study ##
+#################
+
+shootings.Cause <- shootings %>% 
+  group_by(Cause) %>% 
+  count() %>% 
+  ungroup() %>% 
+  mutate(ShootingsPct=`n`/sum(`n`)) %>% 
+  mutate(Shootings=`n`) %>% 
+  arrange(desc(Cause))
+shootings.Cause$Label <- scales::percent(shootings.Cause$ShootingsPct)
+
+ggplot(shootings.Cause, 
+       aes(x = reorder(Cause, -ShootingsPct),
+           y = ShootingsPct)) + 
+  geom_bar(stat = "identity", 
+           fill = "lightblue", 
+           color = "black") +
+  geom_text(aes(label = Label), 
+            hjust = -0.15) +
+  scale_y_continuous(labels = percent) +
+  labs(x = "Cause", 
+       y = "Percent", 
+       title  = "US Shootings Frequencies By Cause") +
+  coord_flip()
+
+#######################
+## Weapon Type study ##
+#######################
+
+shootings.WeaponType <- shootings %>% 
+  group_by(Weapon.Type) %>% 
+  count() %>% 
+  ungroup() %>% 
+  mutate(ShootingsPct=`n`/sum(`n`)) %>% 
+  mutate(Shootings=`n`) %>% 
+  arrange(desc(Weapon.Type))
+shootings.WeaponType$Label <- scales::percent(shootings.WeaponType$ShootingsPct)
+
+ggplot(shootings.WeaponType, 
+       aes(x = reorder(Weapon.Type, -ShootingsPct),
+           y = ShootingsPct)) + 
+  geom_bar(stat = "identity", 
+           fill = "lightblue", 
+           color = "black") +
+  geom_text(aes(label = Label), 
+            hjust = -0.15) +
+  scale_y_continuous(labels = percent) +
+  labs(x = "Type of weapon", 
+       y = "Percent", 
+       title  = "US Shootings Frequencies By Weapon Type") +
+  coord_flip()
+
+################################
+## Mental Health Issues study ##
+################################
+
+shootings.MentalHealthIssues <- shootings %>% 
+  group_by(Mental.Health.Issues) %>% 
+  count() %>% 
+  ungroup() %>% 
+  mutate(ShootingsPct=`n`/sum(`n`)) %>% 
+  mutate(Shootings=`n`) %>% 
+  arrange(desc(Mental.Health.Issues))
+shootings.MentalHealthIssues$Label <- scales::percent(shootings.MentalHealthIssues$ShootingsPct)
+
+ggplot(shootings.MentalHealthIssues, 
+       aes(x = reorder(Mental.Health.Issues, -ShootingsPct),
+           y = ShootingsPct)) + 
+  geom_bar(stat = "identity", 
+           fill = "lightblue", 
+           color = "black") +
+  geom_text(aes(label = Label), 
+            hjust = -0.15) +
+  scale_y_continuous(labels = percent) +
+  labs(x = "Mental disease", 
+       y = "Percent", 
+       title  = "US Shootings Frequencies By mental disease") +
+  coord_flip()
+
+###############################
+## Ten Casualities Min study ##
+###############################
+
+shootings.TenCasualitiesMin <- shootings %>% 
+  group_by(Ten.Casualities.Min) %>% 
+  count() %>% 
+  ungroup() %>% 
+  mutate(ShootingsPct=`n`/sum(`n`)) %>% 
+  mutate(Shootings=`n`) %>% 
+  arrange(desc(Ten.Casualities.Min))
+shootings.TenCasualitiesMin$Label <- scales::percent(shootings.TenCasualitiesMin$ShootingsPct)
+
+ggplot(shootings.TenCasualitiesMin, aes(x="", y=ShootingsPct, fill=Ten.Casualities.Min))+
+  geom_bar(width = 1, stat = "identity") +
+  coord_polar("y", start=0) +
+  theme(axis.text.x=element_blank(), axis.title.y = element_blank()) +
+  geom_text(aes(x=1, y = cumsum(ShootingsPct) - ShootingsPct/3, label=Label)) +
+  ggtitle("US Shootings Frequencies By Severity")
+
+#################
+## State study ##
+#################
 
 shootings.State <- shootings %>% 
   group_by(State) %>%
   count() %>%
   ungroup() %>%
-  mutate(Shootings=`n`/sum(`n`)) %>%
+  mutate(ShootingsPct=`n`/sum(`n`)) %>% 
+  mutate(Shootings=`n`) %>%
   arrange(desc(State))
 
 shootings.State.TV <- shootings %>% 
@@ -56,20 +270,6 @@ shootings.State.TV <- shootings %>%
 
 shootings.State$state <- shootings.State$State
 shootings.State.TV$state <- shootings.State.TV$State
-
-ggplot(shootings.Gender, aes(x="", y=Shootings, fill=Gender))+
-  geom_bar(width = 1, stat = "identity") +
-  coord_polar("y", start=0) +
-  theme(axis.text.x=element_blank()) +
-  geom_text(aes(x=1, y = cumsum(Shootings) - Shootings/3, label=Label)) +
-  ggtitle("US Shootings Frequencies By Gender")
-
-ggplot(shootings.Race, aes(x="", y=Shootings, fill=Race))+
-  geom_bar(width = 1, stat = "identity") +
-  coord_polar("y", start=0) +
-  theme(axis.text.x=element_blank()) +
-  geom_text(aes(x=1, y = cumsum(Shootings) - Shootings/3, label=Label)) +
-  ggtitle("US Shootings Frequencies By Ethnicity")
 
 plot_usmap(data = shootings.State, values = "n", color = "black") + 
   scale_fill_continuous(low = "lightgoldenrodyellow", high = "red", name = "Shootings", label = scales::comma) + 
